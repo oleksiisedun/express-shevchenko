ff01865ceb0480ab8142d15d701e91896c825f38
Oleksii Sedun <oleksiisedun@gmail.com>
2025-09-21 16:53:16 +0300

    Add support for all grammatical cases

--- api/index.js
+++ api/index.js
@@ -8,12 +8,36 @@ const app = express();
 
 app.use(express.json());
 
+async function toGrammaticalCase({ grammaticalCase, personData }) {
+  try {
+    if (personData.gender.toLowerCase() == 'ч') personData.gender = 'masculine';
+    else if (personData.gender.toLowerCase() == 'ж') personData.gender = 'feminine';
+
+    switch(grammaticalCase.toLowerCase()) {
+      case 'родовий':
+        return shevchenko.inGenitive(personData);
+      case 'давальний':
+        return shevchenko.inDative(personData);
+      case 'знахідний':
+        return shevchenko.inAccusative(personData);
+      case 'орудний':
+        return shevchenko.inAblative(personData);
+      case 'місцевий':
+        return shevchenko.inLocative(personData);
+      case 'кличний':
+        return shevchenko.inVocative(personData);
+    }
+  } catch (e) {
+    return `${e.name}: ${e.message}`;
+  }
+}
+
 app.get("/", (req, res) => {
   res.send('express-shevchenko');
 });
 
 app.post('/', (req, res) => {
-  shevchenko.inGenitive(req.body).then(result => res.send(result));
+  toGrammaticalCase(req.body).then(result => res.send(result));
 });
 
 module.exports = app;
